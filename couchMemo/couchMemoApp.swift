ff2b074195b7b749c262ace07d2180943f701d8a
Atsushi <choiyaki@gmail.com>
2026-05-27 08:49:57 +0900

    Initial Commit

---
+++ couchMemo/couchMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  couchMemoApp.swift
+//  couchMemo
+//
+//  Created by 吉田篤史 on 2026/05/27.
+//
+
+import SwiftUI
+
+@main
+struct couchMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ couchMemoTests/couchMemoTests.swift
@@ -0,0 +1,16 @@
+//
+//  couchMemoTests.swift
+//  couchMemoTests
+//
+//  Created by 吉田篤史 on 2026/05/27.
+//
+
+import Testing
+
+struct couchMemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
